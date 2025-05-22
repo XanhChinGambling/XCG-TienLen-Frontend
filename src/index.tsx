9b37d9f0fe6fcda26628eb744e66969b53e8871a
meohexa1a <meohexa.1.a@gmail.com>
2025-05-22 11:09:01 +0700

    refactor: Restructure application logic for Discord SDK integration and improve authentication handling

--- src/index.tsx
+++ src/index.tsx
@@ -10,34 +10,40 @@ import "@radix-ui/themes/styles.css";
 import "./styles/index.css";
 import { Theme } from "@radix-ui/themes";
 import Router from "./pages/Router";
+import useAuthContext, { AuthContextProp } from "@/context/AuthContext";
+import { useEffectOnce } from "@/hook/useEffectOnce";
 
 // --- Application ---
 
 const firebaseApplication = initializeApp(FIREBASE_CONFIG, "XanhChinTienLen");
-const firebaseAnalytics = getAnalytics(firebaseApplication);
-
-const application = async () => {
-  try {
-    const discordSdk = new DiscordSDK(API_BASE);
-    await discordSdk.ready();
-
-    const { code } = await discordSdk.commands.authorize({
-      scope: ["identify", "guilds", "applications.commands"],
-      response_type: "code",
-      client_id: API_BASE,
-      prompt: "none",
-      state: "",
-    });
-
-
-  } catch {
-    console.info("This client running in browser!");
-  }
-};
+getAnalytics(firebaseApplication);
 
 // --- Rendering Root ---
 const ApplicationRendering = () => {
-  application();
+  const authContext = useAuthContext();
+
+  const startApp = async () => {
+    try {
+      const discordSdk = new DiscordSDK(API_BASE);
+      await discordSdk.ready();
+
+      const { code } = await discordSdk.commands.authorize({
+        scope: ["identify", "guilds", "applications.commands"],
+        response_type: "code",
+        client_id: API_BASE,
+        prompt: "none",
+        state: "",
+      });
+
+      authContext.login(code);
+    } catch {
+      console.info("This client running in browser!");
+    }
+  };
+
+  useEffectOnce(() => {
+    startApp();
+  });
 
   return <Router />;
 };
